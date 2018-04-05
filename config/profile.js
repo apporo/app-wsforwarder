c29854399f23ca2e7d30436fb24205dbf5d7775e
pnhung177 <pnhung177@acegik.net>
2018-04-05 18:08:49 +0700

    The first commit

---
+++ config/profile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  newFeatures: {
+    appWsforwarder: {
+      logoliteEnabled: true,
+      sandboxConfig: true
+    }
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = require('devebot').registerLayerware({}, [
+  'app-websocket'
+]);
